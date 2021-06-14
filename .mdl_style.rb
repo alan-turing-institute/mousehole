6328637c57b84be4a07c4b9a116b4183f35c1270
Jim Madge <jmadge@turing.ac.uk>
2021-06-14 14:22:34 +0100

    Correct typos in mdl style

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -9,10 +9,10 @@ rule 'MD003', :style => :atx
 # Only allow dashes in unordered lists
 rule 'MD004', :style => :dash
 
-# Enfore line length of 80 characters except in code blocks and tables
+# Enforce line length of 80 characters except in code blocks and tables
 rule 'MD013', :code_blocks => false, :tables => false
 
-# Ignore blockquotes seperated only be a blank line. This is a limitation of
+# Ignore blockquotes separated only be a blank line. This is a limitation of
 # some markdown parsers, not markdown itself.
 exclude_rule 'MD028'
 
