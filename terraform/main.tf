0008aededb228b6846ded4c27d21c5d4e98321f0
Jim Madge <jmadge@turing.ac.uk>
2021-06-23 14:49:21 +0100

    Move virtual networking configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,22 +4,6 @@ resource "azurerm_resource_group" "this" {
   location = var.location
 }
 
-# Create virtual network
-resource "azurerm_virtual_network" "this" {
-  name                = local.resource_tag.virtual_network
-  address_space       = ["10.1.0.0/16"]
-  location            = var.location
-  resource_group_name = azurerm_resource_group.this.name
-}
-
-# Create subnet
-resource "azurerm_subnet" "this" {
-  name                 = local.resource_tag.subnet
-  resource_group_name  = azurerm_resource_group.this.name
-  virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["10.1.0.0/16"]
-}
-
 # Create Network Security Group
 resource "azurerm_network_security_group" "guacamole" {
   name                = "${local.resource_tag.network_security_group}_guacamole"
