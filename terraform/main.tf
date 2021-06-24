d52a8ea9f49b386dc48f8ce28cb15151c5f70f72
Jim Madge <jmadge@turing.ac.uk>
2021-06-24 14:31:08 +0100

    Move disk resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,22 +43,3 @@ module "virtual_machines" {
   private_ip_address = each.value.private_ip_address
   nsg_rules = each.value.nsg_rules
 }
-
-# Create shared data disk
-resource "azurerm_managed_disk" "this" {
-  name                = "${local.resource_tag.data_disk}_shared"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.this.name
-
-  storage_account_type = var.storage_type
-  create_option        = "Empty"
-  disk_size_gb         = var.shared_disk_size_gb
-}
-
-# Attach shared data disk to DSVM
-resource "azurerm_virtual_machine_data_disk_attachment" "this" {
-  managed_disk_id    = azurerm_managed_disk.this.id
-  virtual_machine_id = module.virtual_machines["dsvm"].virtual_machine.id
-  lun                = "0"
-  caching            = "ReadWrite"
-}
