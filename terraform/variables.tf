40a7dc830fd7517f2d949e55216df1687c7a86fd
Jim Madge <jmadge@turing.ac.uk>
2021-06-24 14:28:45 +0100

    Move resource tag declarations

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -70,19 +70,3 @@ variable "egress_share_size_gb" {
     error_message = "The shared disk size must be a positive integer."
   }
 }
-
-locals {
-  resource_tag = {
-    resource_group         = "RG_${var.sre_name}"
-    virtual_network        = "VNET_${var.sre_name}"
-    subnet                 = "SUBNET_${var.sre_name}"
-    public_ip              = "PUBIP_${var.sre_name}"
-    network_security_group = "NSG_${var.sre_name}"
-    network_interface      = "NIC_${var.sre_name}"
-    virtual_machine        = "VM_${var.sre_name}"
-    os_disk                = "OSDISK_${var.sre_name}"
-    data_disk              = "DATADISK_${var.sre_name}"
-    storage_account        = "STORAGE${var.sre_name}"
-    storage_share          = "SHARE_${var.sre_name}"
-  }
-}
