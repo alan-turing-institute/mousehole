1e551b0f738864f1f8bf972796d7ee5ecfe921fa
Jim Madge <jim@jmadge.com>
2022-03-23 14:45:49 +0000

    Add storage endpoint to subnet

--- terraform/virtual_network.tf
+++ terraform/virtual_network.tf
@@ -12,5 +12,6 @@ resource "azurerm_subnet" "this" {
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.1.0.0/16"]
+  service_endpoints    = ["Microsoft.Storage"]
 }
 
